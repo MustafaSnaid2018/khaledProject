c34b5cb883a724a271b71b534f6eb79c77338a44
MustafaSnaid2018 <39595514+MustafaSnaid2018@users.noreply.github.com>
2018-12-13 11:49:19 +0200

    MOOD960453test5

---
+++ src/test/java/MOOD960453test5.java
@@ -0,0 +1,75 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class MOOD960453Test5 {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.katalon.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testMOOD960453Test5() throws Exception {
+    driver.get("http://localhost/MiTAP.AdminWebSite/TestCase/Edit/960453?ProjectID=2061&GroupID=99346&ProjectName=khaledProject");
+    driver.findElement(By.id("topbar")).click();
+    driver.findElement(By.id("summary")).click();
+    driver.findElement(By.id("summary")).clear();
+    driver.findElement(By.id("summary")).sendKeys("test6");
+    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='×'])[3]/following::div[2]")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
